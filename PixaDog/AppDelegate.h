829c2871ad683bdc83dd02ec5fdc8e3ec7799395
alimir1 <contact@alimir.io>
2017-10-06 12:14:30 -0700

    Initial Commit

---
+++ PixaDog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PixaDog
+//
+//  Created by Ali Mir on 10/6/17.
+//  Copyright © 2017 com.AliMir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PixaDog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PixaDog
+//
+//  Created by Ali Mir on 10/6/17.
+//  Copyright © 2017 com.AliMir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
